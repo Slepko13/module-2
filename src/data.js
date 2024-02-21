bf81751cbae78758a3f388ba5d76233f01546dca
Mykola Slepko <slepko13@gmail.com>
2024-02-21 21:32:59 +0200

    init

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const invoices = [
+  {
+    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    name: "Delba de Oliveira",
+    image_url:
+      "https://cdn.iconscout.com/icon/free/png-512/free-avatar-369-456321.png?f=webp&w=256",
+    email: "delba@oliveira.com",
+    amount: 15695,
+  },
+  {
+    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    name: "Lee Robinson",
+    image_url:
+      "https://cdn.iconscout.com/icon/free/png-512/free-avatar-367-456319.png?f=webp&w=256",
+    email: "lee@robinson.com",
+    amount: 56795,
+  },
+  {
+    id: "3958dc9e-737f-4377-85e9-fec4b6a6442a",
+    name: "Hector Simpson",
+    image_url:
+      "https://cdn.iconscout.com/icon/free/png-512/free-avatar-370-456322.png?f=webp&w=256",
+    email: "hector@simpson.com",
+    amount: 15,
+  },
+  {
+    id: "50ca3e18-62cd-11ee-8c99-0242ac120002",
+    name: "Steven Tey",
+    image_url:
+      "https://cdn.iconscout.com/icon/free/png-512/free-avatar-376-456328.png?f=webp&w=256",
+    email: "steven@tey.com",
+    amount: 157195,
+  },
+  {
+    id: "3958dc9e-787f-4377-85e9-fec4b6a6442a",
+    name: "Steph Dietz",
+    image_url:
+      "https://cdn.iconscout.com/icon/free/png-512/free-avatar-379-456331.png?f=webp&w=256",
+    email: "steph@dietz.com",
+    amount: 4587,
+  },
+];
